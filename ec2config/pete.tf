ad9a91fcd8ded63f8e24117089b31c1c1f44ef4b
peterj1974 <peterj1974@yahoo.co.uk>
2021-05-27 05:56:04 +0100

    Create pete.tf

---
+++ ec2config/pete.tf
@@ -0,0 +1,22 @@
+
+#################################
+##			Variables		   ##
+#################################
+variable "access_key" {}
+variable "secret_key" {
+	type = string
+}
+
+#################################
+##			Provider		   ##
+#################################
+provider "aws" {
+	region = "eu-west-2"
+	access_key = "${var.access_key}"
+	secret_key = "${var.secret_key}"
+}
+
+resource "aws_instance" "instance_7" {
+  ami           = "ami-0ad8ecac8af5fc52b"
+  instance_type = "t2.micro"
+}
